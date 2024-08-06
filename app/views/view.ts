78977cb8bf11ed7299f66fe33654dfaaa3c1d491
Diana Avila <missernightangel@gmail.com>
2024-08-06 18:51:23 -0300

    up

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,14 +5,14 @@ export abstract class View <T>{
   constructor(seletor: string, escapar?: boolean){
     const elemento = document.querySelector(seletor);
     // prevenindo códigos maliciosos
-      if (elemento) {
-        this.elemento = elemento as HTMLElement;
-      } else {
-        throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-      }
-      if (escapar) {
-        this.escapar = escapar;
-      }
+    if (elemento) {
+      this.elemento = elemento as HTMLElement;
+    } else {
+      throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+    }
+    if (escapar) {
+      this.escapar = escapar;
+    }
   }
 
   protected abstract template(model: T): string;
