991b6ecc9b56be5d10291f5844b33d50668d71b7
Diana Avila <missernightangel@gmail.com>
2024-08-06 17:49:24 -0300

    up

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,11 +3,11 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes{
   private negociacoes: Negociacao [] = []; // ou private negociacoes: Array<Negociacao> = []
 
-  adiciona(negociacao: Negociacao) {
+  public adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
-  lista(): readonly Negociacao[] { // ou lista(): ReadonlyArray<Negociacao>{
+  public lista(): readonly Negociacao[] { // ou lista(): ReadonlyArray<Negociacao>{
     return this.negociacoes;
   }
 }
--- app/views/view.ts
+++ app/views/view.ts
@@ -1,15 +1,13 @@
-export class View <T>{
+export abstract class View <T>{
   protected elemento: HTMLElement;
 
   constructor(seletor: string){
     this.elemento = document.querySelector(seletor);
   }
 
-  template(model: T): string{
-    throw Error('Sobreescrever na classe filha');
-  }
+  protected abstract template(model: T): string;
 
-  update(model: T): void{
+  public update(model: T): void{
     const template = this.template(model);
     this.elemento.innerHTML = template;
   }
